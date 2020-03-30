4cc7c2dda2d4e84f5c95bb71a6063901ca6ef90d
Melanie Panem <melanie.panem@creditone.com>
2020-03-30 12:04:27 -0700

    Added Other Resources area and js file

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// DECLARE
+var betterhelpImg = document.getElementById('betterhelp-toggle');
+var betterhelpImgLink = document.getElementById('betterhelp-link');
+
+console.log('testing running');
+
+// FUNKY FUNCTIONS
+betterhelpImg.style.display = "none";
+
+betterhelpImgLink.addEventListener('click', function() {
+	if (betterhelpImg.style.display === "none") {
+		betterhelpImg.style.display = "block";
+		betterhelpImgLink.innerHTML = "Click to close screenshot view."
+	} else {
+		betterhelpImg.style.display = "none";
+		betterhelpImgLink.innerHTML = "Click to see screenshot of BetterHelp approximate subscription costs."
+	}
+});
